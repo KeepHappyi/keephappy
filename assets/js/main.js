3895c9c2cab3800e0afd0dc902783af5f1b0dfc0
KeepHappyi <2362760448@qq.com>
2022-04-17 16:37:17 +0800

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -330,19 +330,5 @@
       time: 3000
     });
 
-  document.documentElement.addEventListener('touchstart', function (event) {
-  if (event.touches.length > 1) {
-    event.preventDefault();
-  }
-  }, false);
-  var lastTouchEnd = 0;
-document.documentElement.addEventListener('touchend', function (event) {
-  var now = Date.now();
-  if (now - lastTouchEnd <= 300) {
-    event.preventDefault();
-  }
-  lastTouchEnd = now;
-}, false);
-
 
 })(jQuery);
